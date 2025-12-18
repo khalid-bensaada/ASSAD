afd3ccc3534e618563df4843e3fa29e3275a59d5
khalid-bensaada <kbensaada953@gmail.com>
2025-12-18 12:58:37 +0100

    added part of check the user if he have accont in the web site

--- pages/login.php
+++ pages/login.php
@@ -17,7 +17,24 @@ if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    $result = $conn->query("SELECT * FROM assad_users WHERE email = $email");
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role'] === 'admin') {
+                header("Location : admin.php");
+            } else {
+                header("Location : dign.php");
+            }
+            exit();
+        }
+
+    }
     
+}
 ?>
 <!DOCTYPE html>
 
